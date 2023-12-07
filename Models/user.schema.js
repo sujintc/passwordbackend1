f25dc4a3ec11f55d5271a98c07ba076465838aed
sujintc <133184229+sujintc@users.noreply.github.com>
2023-12-07 12:04:59 +0530

    Add files via upload

---
+++ Models/user.schema.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+
+const userSchema = mongoose.Schema({
+
+
+    
+    username: String,
+    emailid: String,
+    password: String,
+resetPasswordToken: String,
+resetPasswordTokenExpiery: String,
+
+
+
+});
+
+
+const userModel = mongoose.model('userModel', userSchema);
+
+
+export default userModel;
